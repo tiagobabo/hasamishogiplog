1d915cb5ad849825de1cef5301099bc422b006cf
inpanzinator <inpanzinator@08aa202a-918e-2182-8858-2a3360d0fcb0>
2010-09-30 17:23:55 +0000

    Desenho do tabuleiro melhorado.

--- hasamiShogi.pl
+++ hasamiShogi.pl
@@ -1,8 +1,10 @@
-%tabuleiro
+%DESENHAR O TABULEIRO
 
+linhaLimite:-printLinha([' ',*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,' ']).
+linhaLetras:-printLinha([' ',' ',' ',' ',' ',' ','A',' ',' ',' ',' ',' ',' ',' ','B',' ',' ',' ',' ',' ',' ',' ','C',' ',' ',' ',' ',' ',' ',' ','D',' ',' ',' ',' ',' ',' ',' ','E',' ',' ',' ',' ',' ',' ',' ','F',' ',' ',' ',' ',' ',' ',' ','G',' ',' ',' ',' ',' ',' ',' ','H',' ',' ',' ',' ',' ',' ',' ','I',' ',' ',' ',' ',' ']).
+linhaLetrasV(['1','2','3','4','5','6','7','8','9']).
 
-linhaLimite([*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*,*]).
-linhaDivH([*,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,*]).
+linhaDivH:-printLinha([' ',*,' ',-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,' ',*]).
 
 tabuleiro(
 	 [[1,1,1,1,1,1,1,1,1],
@@ -15,80 +17,67 @@ tabuleiro(
 	  [0,0,0,0,0,0,0,0,0],
 	  [2,2,2,2,2,2,2,2,2]]).
 
-piece1(1, ' ----- ').
-piece1(2, ' ----- ').
-piece1(0, '       ').
+piece1(1):- write(' ----- |').
+piece1(2):- write(' ----- |').
+piece1(0):- write('       |').
 
 
-piece2(1, '|  a  |').
-piece2(2, '|  b  |').
-piece2(0, '       ').
+piece2(1):- write('|  a  ||').
+piece2(2):- write('|  b  ||').
+piece2(0):- write(('       |')).
 
 
-piece3(1, ' ----- ').
-piece3(2, ' ----- ').
-piece3(0, '       ').
-
-
+piece3(1):- write(' ----- |').
+piece3(2):- write(' ----- |').
+piece3(0):- write('       |').
 
 printLinhaPeca([]).
 printLinhaPeca([A|R]):-
-	piece1(A, X),
-	write(X),
-	write('|'),
+	piece1(A),
 	printLinhaPeca(R).
 
 printLinhaPeca3([]).
 printLinhaPeca3([A|R]):-
-	piece3(A, X),
-	write(X),
-	write('|'),
+	piece3(A),
 	printLinhaPeca3(R).
 
 printLinhaPeca2([]).
 printLinhaPeca2([A|R]):-
-	piece2(A, X),
-	write(X),
-	write('|'),
+	piece2(A),
 	printLinhaPeca2(R).
 
 
 printLinha([]).
 printLinha([A|R]):-
 	write(A),
-	write(' '),
 	printLinha(R).
 
 
-printPecas([]).
-printPecas([A|R]):-
-	linhaDivH(X),
-	write('*|'),
+printPecas([],[]).
+printPecas([A|R],[X|Y]):-
+	write(' *|'),
 	printLinhaPeca(A),
-	write('*'),
-	nl,
+	write('*'), nl,
+	write(X),
 	write('*|'),
 	printLinhaPeca2(A),
-	write('*'),
-	nl,
-	write('*|'),
+	write('*'), nl,
+	write(' *|'),
 	printLinhaPeca3(A),
-	write('*'),
-	nl,
-	printLinha(X),
-	nl,
-	printPecas(R).
+	write('*'), nl,
+	linhaDivH, nl,
+	printPecas(R, Y).
 
 desenha:-
 	tabuleiro(T),
-	linhaLimite(L),
-	linhaDivH(X),
-	printLinha(L),
-	nl,
-	printLinha(X),
-	nl,
-	printPecas(T),
-	printLinha(L).
+	linhaLetrasV(X),
+	linhaLetras, nl,
+	linhaLimite, nl,
+	linhaDivH, nl,
+	printPecas(T,X),
+	linhaLimite.
+
+%FIM DO DESENHO DO TABULEIRO
 
 
 
